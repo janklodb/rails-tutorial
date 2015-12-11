ce269d3b8f0f1494720c56df0414d003520cc6e0
JanklodB <janklodb@gmail.com>
2015-12-11 15:55:41 +0000

    Elimintate static _pages repetition (excersise 3.1) and create Contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   
+  get '/contact' => 'static_pages#contact'
 
- 
-
-  get 'static_pages/help'
+  get '/help' => 'static_pages#help'
   
-  get 'static_pages/about'
+  get '/about' => 'static_pages#about'
 
   root 'static_pages#home'
   
