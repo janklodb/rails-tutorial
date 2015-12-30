cc9fa5e8e601679875f8f2e49b8b282a7836a1e1
JanklodB <janklodb@gmail.com>
2015-12-30 13:44:47 +0000

    Finish login/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
+  get 'sessions/new'
+
   resources :users
 
   get 'signup' => 'users#new'
